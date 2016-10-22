a485376863a165dc638e5c8174d85a057c156141
Maciek Sufa <maciej.sufa@gmail.com>
2016-10-22 14:14:21 +0200

    use page generators for clarity

--- src/get_posts.py
+++ src/get_posts.py
@@ -3,6 +3,7 @@ import json
 import facebook
 import elasticsearch
 import requests
+import itertools
 
 def fb_to_es(fb):
 	es = {}
@@ -17,24 +18,26 @@ def process_data(data):
 		id, doc = fb_to_es(post)
 		es.index('hbc', 'post', json.dumps(doc), id)
 
-def get_next_feed(feed):
-	next = feed['paging'].get('next') if 'paging' in feed else None
+def get_next_page(feed):
+	next = feed['paging']['next']
 	print 'next feed URL: {0}'.format(next)
-	return requests.get(next).json() if next else None
+	return requests.get(next).json()
 
-def crawl_remaining_feeds(initial_feed):
-	next_feed = get_next_feed(initial_feed)
-	page = 2
-	while next_feed:
-		print 'processing page: {0}'.format(page)
-		process_data(next_feed['data'])
-		next_feed = get_next_feed(next_feed)
-		page += 1
+def has_more_data(feed):
+	return ('paging' in feed) and ('next' in feed['paging'])
+
+def pages(feed):
+	while has_more_data(feed):
+		feed = get_next_page(feed)
+		yield feed
 
 
 graph = facebook.GraphAPI(access_token=os.environ['FB_TOKEN'], version='2.7')
 es = elasticsearch.Elasticsearch(os.environ['ES_HOST'])
-initial_feed = graph.request(os.environ['FB_GROUP'] + '/feed', {'fields': 'id,created_time,from,message'})
+feed = graph.request(os.environ['FB_GROUP'] + '/feed', {'fields': 'id,created_time,from,message'})
 
-process_data(initial_feed['data'])
-crawl_remaining_feeds(initial_feed)
+# chain the initial feed and the following pages so that we can treat them as
+# a single sequence and don't need to duplicate the logging and processing calls
+for page_index, page in enumerate(itertools.chain([feed], pages(feed)), start=1):
+	print 'processing page: {0}'.format(page_index)
+	process_data(page['data'])
